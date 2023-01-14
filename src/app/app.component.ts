e194c784aa2eee7bded6ddaf9241f3de1c34c14b
OmarBenBrahim <58815340+OmarBenBrahim@users.noreply.github.com>
2023-01-14 23:02:01 +0100

    Create auth and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  users: any;
+
+  constructor(private http: HttpClient, private accountService : AccountService){
+
+  }
+  ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:7125/api/Users').subscribe({
+      next: response => this.users = response,
+      error : error => console.log(error),
+      complete : () => console.log('Request has completed')
+    })
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
 }
